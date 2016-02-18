b9bf18588296cc98c7138ccb8537b55065f91eb6
KRISHNADAS V <krishnadaskdkd@gmail.com>
2016-02-18 17:52:52 +0530

    simple calculator angularJs

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 var app = angular.module('app', []);
 app.controller('ctrl', ['$scope', function ($scope) {
-            $scope.view = "0";
-            $scope.nums = [[7, 8, 9], [4, 5, 6], [1, 2, 3]];
-    
-            $scope.btnClick = function (n) {
-                if (n == '0') {
-                    if ($scope.view != '0') {
-                        $scope.view += n;
-                    }
-                }
-                else if(n=='c'){
-                    $scope.view="0";
-                }
-                else if(n=='.'){
-                    if($scope.view.indexOf(".")==-1){
-                        $scope.view += n;
-                    }
-                }
-                else {
-                    if ($scope.view != '0') {
-                        $scope.view += n;
-                    }
-                    else{
-                        $scope.view=""+n;
-                    }
-                }
-                };
-
-
-            }])
+    $scope.nums = [[7, 8, 9], [4, 5, 6], [1, 2, 3]];
+    $scope.expression = "";
+    $scope.opEnable = false;
+    $scope.pointEnable = true;
+    $scope.minusEnable = true;
+    var pointadded = false;
+    var numadded = false;
+    $scope.numClick = function (n) {
+        $scope.expression += n;
+        $scope.opEnable = true;
+        $scope.minusEnable = true;
+        pointadded = false;
+        numadded = true;
+    };
+    $scope.fnClick = function (n) {
+        if (pointadded == true) {
+            $scope.expression += '0';
+        }
+        if ($scope.expression != "") {
+            $scope.expression += n;
+            $scope.opEnable = false;
+            $scope.pointEnable = true;
+            $scope.minusEnable = false;
+            numadded = false;
+        } else {
+            if (n == '-') {
+                $scope.expression += n;
+                $scope.minusEnable = false;
+            }
+        }
+    };
+    $scope.pntClick = function () {
+        if (numadded == false) {
+            $scope.expression += 0;
+        }
+        $scope.expression += '.';
+        $scope.pointEnable = false;
+        pointadded = true;
+    };
+    $scope.ansClick = function () {
+        var ans = eval($scope.expression);
+        $scope.expression = "" + ans;
+    }
+    $scope.clrClick=function() {
+        $scope.expression = "";
+        $scope.opEnable = false;
+        $scope.pointEnable = true;
+        $scope.minusEnable = true;
+        pointadded = false;
+        numadded = false;
+    }
+}])
